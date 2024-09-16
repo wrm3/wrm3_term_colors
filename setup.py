012d2b3f91afc22465bce0192221a8b086baccaf
wrm3 <wrmartel3@gmail.com>
2024-09-16 14:18:59 -0400

    version update

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
-
 setup(
-    name='wrm3_term_colors',
-    version='0.1.0',
+    name='wrm3_term_colors',  # The name you chose, check it's unique on PyPI
+    version='0.1.1',
     packages=find_packages(),
-    install_requires=[
-        # Add your dependencies here, e.g.,
-        # 'colorama>=0.4.4',
-    ],
+    install_requires=[],  # List any dependencies here
     author='WRM3',
     author_email='wrmartel3@gmail.com',
     description='A terminal printing library that adds more color options.',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/wrm3/wrm3_term_colors',
+    url='https://github.com/wrm3/wrm3_term_colors.git',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
 )
